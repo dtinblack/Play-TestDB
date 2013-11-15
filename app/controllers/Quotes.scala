17a32d1bb3cf6572a473eeba834250d452f07301
David Black <dtinblack@gmail.com>
2013-11-15 08:38:33 +0000

    added input form

--- app/controllers/Quotes.scala
+++ app/controllers/Quotes.scala
@@ -3,16 +3,65 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import models.Quote
 
+import play.api.data.Form
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
+import play.api.i18n.Messages
+
+import play.api.mvc.Flash
+
 object Quotes extends Controller {
   
-  def list = Action { implicit request => 
-    
-    val quotes = Quote.findAll
-    
-    Ok(views.html.quotes.list(quotes))
+  private val quoteForm: Form[Quote] = Form(
+      mapping(
+        "id" -> longNumber.verifying(
+            "validation.id.duplicate", Quote.findById(_).isEmpty),
+         "text" -> nonEmptyText,
+         "author" -> nonEmptyText
+         ) (Quote.apply)(Quote.unapply)   
+   )
   
+  
+  def list = Action { implicit request => 
+     val quotes = Quote.findAll
+     Ok(views.html.quotes.list(quotes))
   }
   
+    def show(id: Long) = Action { implicit request =>
+    
+    Quote.findById(id).map{ quote => 
+      Ok(views.html.quotes.details(quote))
+     }.getOrElse(NotFound)  
+  }
   
-
+  def save = Action { implicit request =>
+    
+     val newQuoteForm = quoteForm.bindFromRequest()
+     
+     newQuoteForm.fold(
+         hasErrors = { form =>
+           Redirect(routes.Quotes.newQuote()) .
+            flashing(Flash(form.data) + 
+                ("error" -> Messages("validation.errors")))   
+         },
+         
+         success = { newQuote => 
+           Quote.add(newQuote)
+           val message = Messages("quotes.new.success", newQuote.text)
+           Redirect(routes.Quotes.show(newQuote.id)) .
+            flashing("success" -> message)                    
+         }
+      )   
+  }
+  
+  def newQuote = Action { implicit request =>
+   
+     val form = if (flash.get("error").isDefined)
+          quoteForm.bind(flash.data)
+     else
+          quoteForm
+       
+       Ok(views.html.quotes.editQuote(form))
+    
+  }
+  
 }
--- app/models/Quote.scala
+++ app/models/Quote.scala
@@ -2,17 +2,28 @@ package models
 
 case class Quote (id: Long, text: String, author: String)
 
-object Quote {
+object Quote{
   
-  val quotes = Set (
-    
-    Quote(1L, "The World is everything", "Ludwig Wittgenstein"),
-    Quote(2L, "Tomorrow is another day", "Scarlet O'Hara"),
-    Quote(3L, "It never rains but it pours", "Annonymous")
-       
+  
+ var quotes = Set (
+      
+   Quote(1, "The world is everything",
+    "Ludwig Wittgenstien"),
+   Quote(2, "Tomorrow is another day",
+    "Scarlet O'Hara"),
+   Quote(3, "It never rains but it pours", "Annonymous")
+     
   )
-    
-  def findAll = quotes.toList.sortBy(_.id)
+  
+ def findAll = quotes.toList.sortBy(_.id)
+ 
+ def findById( id: Long) = quotes.find(_.id == id)
+ 
+ def add( quote: Quote ) {
+   
+      quotes = quotes + quote
+   }
+   
 }
 
 
